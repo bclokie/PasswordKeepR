a0ce9b44727775d4f1087f9e85cc4a70de32158e
Chris Baltis <cbaltis@gmail.com>
2023-02-12 15:22:47 +0000

    installed cookie session and bcrypt

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ require('dotenv').config();
 const sassMiddleware = require('./lib/sass-middleware');
 const express = require('express');
 const morgan = require('morgan');
+const cookieSession = require('cookie-session');
+const bcrypt = require("bcryptjs");
+
 
 const PORT = process.env.PORT || 8080;
 const app = express();
@@ -25,6 +28,10 @@ app.use(
   })
 );
 app.use(express.static('public'));
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1', 'key2'],
+}));
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
