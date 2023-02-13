11cc60272641c09e5de34f2eb6fe4ce2861f52bf
Chris Baltis <cbaltis@gmail.com>
2023-02-13 21:13:40 +0000

    got manager post route working

--- routes/manager.js
+++ routes/manager.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 const userQueries = require("../db/queries/users");
 
 // main page rendering
@@ -10,21 +10,18 @@ router.get("/", (req, res) => {
 // Create a new password
 router.post("/", (req, res) => {
   const passwordInfo = req.body;
-  console.log(passwordInfo)
   userQueries
     .addPassword(passwordInfo)
-    .then((passwordInfo) => {
-      if (!passwordInfo) {
+    .then((data) => {
+      console.log("## data", data);
+      if (!data) {
         res.send({ error: "error" });
         return;
       }
-      req.session.userId = user.id;
-      return res.send("🤗");
+      // req.session.userId = user.id;
+      return res.send("hi");
     })
     .catch((e) => res.send(e));
 });
-
-
-
 
 module.exports = router;
