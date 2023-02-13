264b313a37759c58e73c3ea7cebedfda650b0b60
mingun choi <ryanchoi100826@gmail.com>
2023-02-13 13:42:41 -0500

    nothing

--- db/schema/02_passwords.sql
+++ db/schema/02_passwords.sql
@@ -1,19 +1,14 @@
 
--- DROP TABLE IF EXISTS passwords CASCADE;
+DROP TABLE IF EXISTS passwords CASCADE;
 
--- CREATE TABLE passwords (
---   id SERIAL PRIMARY KEY NOT NULL,
---   site_name VARCHAR(255) NOT NUll,
---   site_url VARCHAR(255),
---   site_username VARCHAR(255) NOT NUll,
---   site_password SERIAL PRIMARY KEY NOT NULL,
---   user_id VARCHAR(150),
---   FOREIGN KEY(user_id) REFERENCES users(id),
---   categories_id VARCHAR(150) NOT NULL,
---   FOREIGN KEY(categories_id) REFERENCES users(id)
--- );
-
--- INSERT INTO passwords
--- (site_name, site_url, site_username, site_password, user_id, categories_id)
--- VALUES
--- ('1', 'google', 'www.google.com', 'cmgg919', 1234, 'cmg', );
+CREATE TABLE passwords (
+  id SERIAL PRIMARY KEY NOT NULL,
+  site_name VARCHAR(255) NOT NUll,
+  site_url VARCHAR(255),
+  site_username VARCHAR(255) NOT NUll,
+  site_password SERIAL PRIMARY KEY NOT NULL,
+  user_id VARCHAR(150),
+  FOREIGN KEY(user_id) REFERENCES users(id),
+  categories_id VARCHAR(150) NOT NULL,
+  FOREIGN KEY(categories_id) REFERENCES users(id)
+);
