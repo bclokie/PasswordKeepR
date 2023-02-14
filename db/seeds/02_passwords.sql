3290f5616c787d4d7292667318f7d6f90bb9fb4e
Braden <braden.clokie@gmail.com>
2023-02-14 10:17:48 -0500

    Added more password seeds for /manager visibility

--- db/seeds/02_passwords.sql
+++ db/seeds/02_passwords.sql
@@ -1,4 +1,8 @@
 INSERT INTO passwords
 (site_name, site_url, site_username, site_password)
 VALUES
-('google', 'https://www.google.com/', 'bear', 123);
+('Google', 'https://www.google.com/', 'bear', 123),
+('Reddit', 'https://old.reddit.com/', 'fabio', 'password'),
+('Facebook', 'https://www.facebook.com/', 'oldPerson', 'whatsmypasswordagain'),
+('LinkedIn', 'https://www.linkedin.com/', 'businessman', 'businessplan');
+
